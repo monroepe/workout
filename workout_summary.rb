2904064045c6a4729b150f9cb4a21977ca3c7d22
Peter <monroepe@gmail.com>
2014-09-02 10:38:25 -0400

    First commit

---
+++ workout_summary.rb
@@ -0,0 +1,29 @@
+require_relative 'workout'
+require 'csv'
+
+# create a hash of workout info from CSV
+def load_workout_data(filename)
+  workouts = {}
+
+  CSV.foreach(filename, headers: true, header_converters: :symbol, converters: :numeric) do |row|
+    workout_id = row[:workout_id]
+
+    if !workouts[workout_id]
+      workouts[workout_id] = {
+        date: row[:date],
+        exercises: []
+      }
+    end
+
+    exercise = {
+      name: row[:exercise],
+      category: row[:category],
+      duration_in_min: row[:duration_in_min],
+      intensity: row[:intensity]
+    }
+
+    workouts[workout_id][:exercises] << exercise
+  end
+
+  workouts
+end
